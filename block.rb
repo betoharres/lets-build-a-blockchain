aabeeec375e1b2ce47e7174161cdcd1f53d27dc2
Alberto Harres Rocha <albertohrocha@gmail.com>
2017-11-27 00:15:24 -0200

    Change log

--- block.rb
+++ block.rb
@@ -33,7 +33,7 @@ class Block
       nonce = "HELP I'M TRAPPED IN A NONCE FACTORY"
       count = 0
       until is_valid_nonce?(nonce)
-        puts '.' if count % 100_000 == 0
+        puts 'mining block...' if count % 100_000 == 0
         nonce = nonce.next
         count =+ 1
       end
